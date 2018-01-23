bc2fd80efe131945fbe33dad691bf6e5f80610f9
jochem33 <jochemjuriaan.baas@coderclass.nl>
2018-01-23 10:28:14 +0100

    [add] lopen script (doet het nog niet)

--- Assets/Sprites/Environment/scripts/player.cs
+++ Assets/Sprites/Environment/scripts/player.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour {
+	public float moveSpeed = 10.0f;
+	bool facingRight = true;
+	float move = Input.GetAxis("Horizontal");
+
+	Animator anim;
+	Rigidbody2D rigid;
 
 	// Use this for initialization
 	void Start () {
-		
+		anim = GetComponent<Animator> ();
+		rigid = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void FixedUpdate () {
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+		rigid.velocity = new Vector2 (move * moveSpeed, rigid.velocity.y);
+		if (move > 0 && !facingRight) {
+			FlipFacing ();
+		} else if (move < 0 && facingRight) {
+			FlipFacing ();
+		}
+	}
+
+	void FlipFacing() {
+		facingRight = !facingRight;
+		Vector3 charScale = transform.localScale;
+		charScale.x = charScale.x * -1;
+		transform.localScale = charScale;
 	}
 }
