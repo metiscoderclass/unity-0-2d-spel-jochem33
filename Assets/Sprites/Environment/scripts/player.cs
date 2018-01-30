9bc51a502bb20d9eb575663369b50919debb6b65
jochem33 <jochemjuriaan.baas@coderclass.nl>
2018-01-30 10:02:25 +0100

    [add] springen (maar nog wel dubbeljump)

--- Assets/Sprites/Environment/scripts/player.cs
+++ Assets/Sprites/Environment/scripts/player.cs
@@ -6,6 +6,9 @@ public class player : MonoBehaviour {
 	public float moveSpeed = 10.0f;
 	bool facingRight = true;
 
+	public float jumpSpeed = 300.0f;
+
+
 	Animator anim;
 	Rigidbody2D rigid;
 	float move;
@@ -24,10 +27,15 @@ public class player : MonoBehaviour {
 		move = Input.GetAxis("Horizontal");
 		anim.SetFloat ("speed", Mathf.Abs (move));
 		rigid.velocity = new Vector2 (move * moveSpeed, rigid.velocity.y);
+
 		if (move > 0 && !facingRight) {
 			FlipFacing ();
 		} else if (move < 0 && facingRight) {
 			FlipFacing ();
+		}
+
+		if (Input.GetButtonDown ("Jump")) {
+			rigid.AddForce (Vector2.up * jumpSpeed);
 		}
 	}
 
